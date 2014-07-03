6f1a996d2a08671b7db332608146b5ee486c1d5b
kirk7880 <kirk7880@gmail.com>
2014-07-03 19:37:00 -0400

    Fixed whitespace. Added shrinkwrap

--- benchmark.js
+++ benchmark.js
@@ -6,18 +6,18 @@ var leaderboard = 'persons';
 
 var max = 10000;
 var count = 0;
-var value = 'www.nytimes.com';
+var value = 'www.google.com';
 
 function run() {
-	spacesaver.increment({leaderboard: leaderboard, value: value}, function(e, res) {
-		if (++count >= max) {
-			spacesaver.leaders({leaderboard: leaderboard, k: 2}, function(e, res) {
-				console.log('leaders', e, res);
-				process.exit();
-			});
-		}
-	});
+  spacesaver.increment({leaderboard: leaderboard, value: value}, function(e, res) {
+    if (++count >= max) {
+      spacesaver.leaders({leaderboard: leaderboard, k: 2}, function(e, res) {
+        console.log('leaders', e, res);
+        process.exit();
+      });
+    }
+  });
 }
 
 for (var i=0; i<max; i++) 
-	run();
+  run();

70ef93778ef0b7e0f80b68a17b69b2f9da94d9dc
kirk7880 <kirk7880@gmail.com>
2014-07-21 15:00:41 -0400

    Added comments

--- benchmark.js
+++ benchmark.js
@@ -9,9 +9,9 @@ var count = 0;
 var value = ['http://www.google.com'];
 
 function run() {
-  spacesaver.increment({leaderboard: leaderboard, values: value}).process(function(e, res) {
+  spacesaver.add({leaderboard: leaderboard, values: value}).increment(function(e, res) {
     if (++count >= max) {
-      spacesaver.leaders({leaderboard: leaderboard}, function(e, res) {
+      spacesaver.leaders(leaderboard, null, function(e, res) {
         console.log('leaders', res);
         process.exit();
       });

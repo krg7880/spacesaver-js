1a56a079249f5796ac17d9a602d9dff1b0406105
kirk7880 <kirk7880@gmail.com>
2014-07-21 16:12:42 -0400

    Updated benchmark

--- benchmark.js
+++ benchmark.js
@@ -3,19 +3,19 @@ var SpaceSaver = require('./lib/');
 // Test Leaderboard
 var spacesaver = new SpaceSaver(100);
 var leaderboard = 'topsites';
-var members = ['www.google.com', 'www.nytmes.com', 'www.yahoo.com', 'www.cnn.com'];
-var membersTwo = ['www.facebook.com', 'www.twitter.com', 'www.tumblr.com'];
+var news_members = ['www.google.com', 'www.nytmes.com', 'www.yahoo.com', 'www.cnn.com'];
+var social_members = ['www.facebook.com', 'www.twitter.com', 'www.tumblr.com'];
 
 var max = 1;
 var count = 0;
 
 function run() {
-  spacesaver.add(leaderboard, members)
-    .add('social', membersTwo).increment(function(e, res) {
-      if (++count >= (max * ((members.length + membersTwo.length)-1)) ) {
+  spacesaver.add(leaderboard, news_members)
+    .add('social', social_members).increment(function(e, res) {
+      if (++count >= (max * ((news_members.length + social_members.length)-1)) ) {
         // pick one member to be the leader
-        var idx = Math.floor(Math.random() * (members.length-1));
-        var leader = members[idx];
+        var idx = Math.floor(Math.random() * (news_members.length-1));
+        var leader = news_members[idx];
         spacesaver.add(leaderboard, [leader]).increment(function(e, res) {
           spacesaver.leaders(leaderboard, null, function(e, res) {
             console.log('news', res);
